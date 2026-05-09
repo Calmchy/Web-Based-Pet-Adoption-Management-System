a45916773a9895448bb52c9f0654f79acc913466
Calmchy <chyrilmanatad@gmail.com>
2026-05-09 21:09:18 +0800

    Update

--- www/includes/navbar.php
+++ www/includes/navbar.php
@@ -1,13 +1,40 @@
-<nav>
-    <a href="index.php?page=home">Home</a>
-    <a href="index.php?page=pets">Pets</a>
-    <a href="index.php?page=apply">Apply</a>
+<nav class="navbar">
+    <!-- LEFT -->
+    <div class="nav-left">
 
-    <?php if (isset($_SESSION['user_id'])): ?>
-        <a href="#">Dashboard</a>
-        <a href="actions/logout.php">Logout</a>
-    <?php else: ?>
-        <a href="index.php?page=login">Login</a>
-        <a href="index.php?page=register">Register</a>
-    <?php endif; ?>
+        <!-- Logo + Brand -->
+        <div class="brand-container">
+            <img class="logo" src="../assets/images/logo.png" alt="AdoptME Logo">
+
+            <div class="brand">
+                <h1>AdoptME</h1>
+                <p>Pet Adoption System</p>
+            </div>
+        </div>
+
+        <!-- Nav Links -->
+        <div class="nav-links">
+            <a href="index.php?page=home">Home</a>
+            <a href="index.php?page=pets">Pets</a>
+            <a href="index.php?page=apply">Apply</a>
+            <a href="index.php?page=about">About</a>
+        </div>
+
+    </div>
+
+    <!-- RIGHT -->
+    <div class="nav-right">
+
+        <!-- Dark Mode Button -->
+        <button id="darkModeBtn">🌙</button>
+
+        <?php if (isset($_SESSION['user_id'])): ?>
+            <a href="#">Dashboard</a>
+            <a href="actions/logout.php">Logout</a>
+        <?php else: ?>
+            <a href="index.php?page=login">Login</a>
+            <a href="index.php?page=register" class="register-btn">Register</a>
+        <?php endif; ?>
+
+    </div>
 </nav>
