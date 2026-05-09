ce0ea2f10f76b16db008785b1f5840b71eb86f03
Calmchy <chyrilmanatad@gmail.com>
2026-05-09 21:09:05 +0800

    Add header

--- www/includes/header.php
+++ www/includes/header.php
@@ -5,5 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AdoptMe 🐾</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/navbar.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+    <link rel="stylesheet" href="assets/css/home.css">
+    <link rel="stylesheet" href="assets/css/darkmode.css">
+    <link rel="shortcut icon" href="../assets/images/logo.png" type="image/x-icon">
 </head>
 <body>
+    <header>
+        <?php include "includes/navbar.php"; ?>
+    </header>
+    <script src="assets/js/darkmode.js"></script>
