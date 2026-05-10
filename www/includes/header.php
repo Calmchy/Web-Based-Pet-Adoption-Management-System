dd6472a6a29dc1a6eac0753b6a9745143142ace9
Calmchy <chyrilmanatad@gmail.com>
2026-05-10 19:40:14 +0800

    Update

--- www/includes/header.php
+++ www/includes/header.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="assets/css/home.css">
     <link rel="stylesheet" href="assets/css/darkmode.css">
     <link rel="stylesheet" href="assets/css/auth.css">
-    <link rel="shortcut icon" href="../assets/images/logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../assets/images/logo2.png" type="image/x-icon">
 </head>
 <body>
     <header>
